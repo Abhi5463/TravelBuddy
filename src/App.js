8e2990b6a6c9b78512064e72f98af4220b768883
Abhi5463 <abhishekkumar54635460@gmail.com>
2022-01-28 16:15:10 +0530

    ready to deplo

--- src/App.js
+++ src/App.js
@@ -42,9 +42,11 @@ const App = () => {
 
       getPlacesData(type, bounds.sw, bounds.ne)
         .then((data) => {
+          console.log(data);
           setPlaces(data.filter((place) => place.name && place.num_reviews > 0));
-          setFilteredPlaces([]);
-          setRating('');
+          // setPlaces([]);
+          // setFilteredPlaces([]);
+          // setRating('');
           setIsLoading(false);
         });
     }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,31 +1,31 @@
 import React from 'react';
-import { Autocomplete } from '@react-google-maps/api';
-import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
-import SearchIcon from '@material-ui/icons/Search';
+// import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
+// import SearchIcon from '@material-ui/icons/Search';
 
 import useStyles from './styles.js';
 
-const Header = ({ onPlaceChanged, onLoad }) => {
+const Header = () => {
   const classes = useStyles();
 
   return (
     <AppBar position="static">
       <Toolbar className={classes.toolbar}>
         <Typography variant="h5" className={classes.title}>
-          Travel Advisor
+          Travel Buddy
         </Typography>
         <Box display="flex">
           <Typography variant="h6" className={classes.title}>
             Explore new places
           </Typography>
-          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+          {/* <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
             <div className={classes.search}>
               <div className={classes.searchIcon}>
                 <SearchIcon />
               </div>
               <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
             </div>
-          </Autocomplete>
+          </Autocomplete> */}
         </Box>
       </Toolbar>
     </AppBar>
